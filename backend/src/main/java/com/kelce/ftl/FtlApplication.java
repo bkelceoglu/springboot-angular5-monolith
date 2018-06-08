bdd3b46fbf7e1afa5eb4d1444b8d2b59eecd71de
beyazıt kelçeoğlu <beyazit@kelceoglu.com>
2018-06-08 12:29:16 +0300

    first commit

---
+++ backend/src/main/java/com/kelce/ftl/FtlApplication.java
@@ -0,0 +1,12 @@
+package com.kelce.ftl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtlApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/kelce/ftl/FtlApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kelce.ftl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FtlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
